fd2c4f976ae66ed70a64c2b183e8a2392a28161b
Kevin Richter Krogsgaard Andersen <88718302+Keveren1@users.noreply.github.com>
2022-03-21 13:03:47 +0100

    Add project files.

---
+++ MusicRecordsREST/Manager/MusicRecordManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicRecordsREST.Models;
+
+namespace MusicRecordsREST.Manager
+{
+    public class MusicRecordManager
+    {
+        private static List<MusicRecordsData> data = new List<MusicRecordsData>()
+        {
+            new MusicRecordsData() {Title = "Bodega mama", Artist = "Kim Larsen", Duration = 4, PublicationYear = 2000},
+            new MusicRecordsData() {Title = "Sut den op fra slap", Artist = "Suspekt", Duration = 2, PublicationYear = 2015},
+            new MusicRecordsData() {Title = "Vi maler byen rød", Artist = "Birthe Kjær", Duration = 3, PublicationYear = 1970},
+            new MusicRecordsData() {Title = "In da club", Artist = "50 Cent", Duration = 5, PublicationYear = 2002}
+        };
+
+        public List<MusicRecordsData> GetAll(string? Title, string? Artist, int? Duration, int? PublicationYear)
+        {
+            List<MusicRecordsData> result = new List<MusicRecordsData>(data);
+            
+            if (!string.IsNullOrWhiteSpace(Title))
+            {
+                result = result.FindAll(filterTitle =>
+                    filterTitle.Title.Contains(Title, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (!string.IsNullOrWhiteSpace(Artist))
+            {
+                result = result.FindAll(filterArtist =>
+                    filterArtist.Artist.Contains(Artist, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (Duration != null)
+            {
+                result = result.FindAll(filterDuration => filterDuration.Duration == Duration);
+            }
+
+            if (PublicationYear != null)
+            {
+                result = result.FindAll(filterPublicationYear =>
+                    filterPublicationYear.PublicationYear == PublicationYear);
+            }
+            {
+                
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ MusicRecordsREST/Models/MusicRecordsData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRecordsREST.Models
+{
+    public class MusicRecordsData
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+
+
+
+
+    }
+}
