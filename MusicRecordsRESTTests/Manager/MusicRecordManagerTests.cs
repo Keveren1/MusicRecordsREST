08644816099351d6ed91c1be95c0db3bbdcf2517
Kevin Richter Krogsgaard Andersen <88718302+Keveren1@users.noreply.github.com>
2022-03-21 14:31:55 +0100

    Hejsa

---
+++ MusicRecordsRESTTests/Manager/MusicRecordManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicRecordsREST.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicRecordsREST.Models;
+
+namespace MusicRecordsREST.Manager.Tests
+{
+    [TestClass()]
+    public class MusicRecordManagerTests
+    {
+        private MusicRecordManager _recordManager;
+        [TestInitialize()]
+        public void SetUp()
+        {
+            _recordManager = new MusicRecordManager();
+        }
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.IsNotNull(_recordManager.GetAll(null, null, null, null));
+        }
+    }
+}
